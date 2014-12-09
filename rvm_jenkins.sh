3d5ebd9bf7f4054d91a68c89cb69d58dc1497668
Richard Kent Jordan <richard.jordan@sial.com>
2014-12-08 19:17:02 -0600

    Moved to latest ubuntu. Added the ability to use corporate proxies.

--- rvm_jenkins.sh
+++ rvm_jenkins.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker rm jenkins
-docker run -d -p 8080:8080 --name jenkins -v /home/rjordan2/docker_data/jenkins:/var/jenkins_home rvm_jenkins:latest
+docker run -u root -t -i -p 8080:8080 --name jenkins -v /home/rjordan2/docker_data/jenkins:/var/jenkins_home rvm_jenkins:latest /bin/bash
 
